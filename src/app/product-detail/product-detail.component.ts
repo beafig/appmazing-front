96a5006634b88f249a8e571ba73f96a31784b88e
beafig <beafigueroa89@gmail.com>
2023-11-13 14:41:26 +0100

    Add method to show previous and next products in product-detail

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,24 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductsService } from '../products.service';
-import { ActivatedRoute, Router } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ProductsService } from "../products.service";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
-  selector: 'app-product-detail',
-  templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  selector: "app-product-detail",
+  templateUrl: "./product-detail.component.html",
+  styleUrls: ["./product-detail.component.css"],
 })
 export class ProductDetailComponent implements OnInit {
-
   product: any;
 
-  constructor(private productsService: ProductsService, private route: ActivatedRoute,  private router: Router) { }
+  products: any = [];
+  ids = [];
+  constructor(
+    private productsService: ProductsService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit() {
-    this.productsService.getProduct(this.route.snapshot.params['id']).subscribe(data =>{
-      this.product = data;
-    })
+    this.route.params.subscribe((params) => {
+      this.productsService
+        .getProduct(this.route.snapshot.params["id"])
+        .subscribe((data) => {
+          this.product = data;
+        });
+    });
   }
-  navigateToHome(){
-    this.router.navigate(['/products'])
+  navigateToHome() {
+    this.router.navigate(["/products"]);
+  }
+  async createIdsArray(): Promise<number[]> {
+    return new Promise((resolve) => {
+      this.productsService.getProducts().subscribe((data) => {
+        this.products = data;
+        this.ids = this.products.map((e) => e.id);
+        resolve(this.ids);
+      });
+    });
+  }
+
+  async goToPrevious() {
+    this.ids = await this.createIdsArray();
+    let currentId = parseInt(this.route.snapshot.params["id"]);
+    let currentIndex = this.ids.findIndex((id) => {
+      return id === currentId;
+    });
+    if (currentIndex === 0) {
+      currentIndex = this.ids.length - 1;
+      this.router.navigate(["/product/", this.ids[currentIndex]]);
+    } else {
+      this.router.navigate(["/product/", this.ids[currentIndex - 1]]);
+    }
+  }
+
+  async goToNext() {
+    this.ids = await this.createIdsArray();
+    let currentId = parseInt(this.route.snapshot.params["id"]);
+    let currentIndex = this.ids.findIndex((id) => {
+      return id === currentId;
+    });
+    if (currentIndex === this.ids.length - 1) {
+      this.router.navigate(["/product/", this.ids[0]]);
+    } else {
+      this.router.navigate(["/product/", this.ids[currentIndex + 1]]);
+    }
   }
 }
