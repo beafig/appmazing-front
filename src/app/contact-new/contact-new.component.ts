d7d4c55ce5f6009cf4a13633cee8bd4350aec79c
beafig <beafigueroa89@gmail.com>
2023-11-14 09:30:37 +0100

    Add validators in form contacts and products

--- src/app/contact-new/contact-new.component.ts
+++ src/app/contact-new/contact-new.component.ts
@@ -1,50 +1,63 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ContactsService } from '../contacts.service';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
+import { ContactsService } from "../contacts.service";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-contact-new',
-  templateUrl: './contact-new.component.html',
-  styleUrls: ['./contact-new.component.css']
+  selector: "app-contact-new",
+  templateUrl: "./contact-new.component.html",
+  styleUrls: ["./contact-new.component.css"],
 })
 export class ContactNewComponent implements OnInit {
+  // para que las validaciones del form funcionen correctamente
+  @ViewChild("form", { static: true }) form: NgForm;
   // los campos que tendrá el objeto Contact que enviaremos al servidor.
-name:string;
-first_surname: string;
-second_surname:string;
-phone_number: number;
-email:string;
+  name: string;
+  first_surname: string;
+  second_surname: string;
+  phone_number: number;
+  email: string;
 
-  constructor(private router: Router, private contactsService: ContactsService) { }
+  constructor(
+    private router: Router,
+    private contactsService: ContactsService
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   // handler del botón de submit, creamos el contacto recogiendo todos los valores del form, y llamamos al método para volver a la tabla
-  async newContact(){
-    const contact= {
+  async newContact() {
+    const contact = {
       name: this.name,
       first_surname: this.first_surname,
       second_surname: this.second_surname,
       phone_number: this.phone_number,
-      email: this.email
+      email: this.email,
+    };
+    // validaciones y mensaje de error del form
+    if (
+      this.form.controls["name"].hasError("required") ||
+      this.form.controls["first_surname"].hasError("required") ||
+      this.form.controls["phone_number"].hasError("required") ||
+      this.form.controls["email"].hasError("required") ||
+      this.form.controls["email"].hasError("pattern")
+    ) {
+      alert(
+        "Revisa el formulario para asegurarte de que todos los campos son válidos."
+      );
+    } else {
+      this.contactsService.newContact(contact);
+      await this.navigateToHome();
     }
-    // condicional para controlar que los campos oblicagorios estén cubiertos, si escribo y borro no funciona ya que pasa de undefined a '' string vacío.
-      if(contact.name === undefined || contact.first_surname === undefined  || contact.phone_number === undefined || contact.email === undefined){
-        alert('Todos los campos son obligarios excepto el segundo apellido, revise y cubra los campos que faltan')
-      } else {
-        this.contactsService.newContact(contact);
-        await this.navigateToHome();
-      }
-
   }
-// handler del botón cancelar
-  cancelInsert(){
+
+  // handler del botón cancelar
+  cancelInsert() {
     this.navigateToHome();
   }
 
   //método para volver a la tabla de contactos, tanto cuando crees un nuevo contacto como cuando le des al botón de cancelar, llamaremos a este método en la handler del los botones
-  navigateToHome(){
-    this.router.navigate(['/contacts'])
+  navigateToHome() {
+    this.router.navigate(["/contacts"]);
   }
 }
--- src/app/product-new/product-new.component.ts
+++ src/app/product-new/product-new.component.ts
@@ -1,69 +1,82 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { ProductsService } from '../products.service';
-import { CategoriesService } from '../categories.service';
-import { Product } from 'model/Product';
-import { Category } from 'model/Category';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
+import { ProductsService } from "../products.service";
+import { CategoriesService } from "../categories.service";
+import { Product } from "model/Product";
+import { Category } from "model/Category";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-product-new',
-  templateUrl: './product-new.component.html',
-  styleUrls: ['./product-new.component.css']
+  selector: "app-product-new",
+  templateUrl: "./product-new.component.html",
+  styleUrls: ["./product-new.component.css"],
 })
 export class ProductNewComponent implements OnInit {
+  @ViewChild("form", { static: true }) form: NgForm;
+
   // product va a ser de tipo Product, y viene del modelo Product.ts
-product: Product = new Product();
-category: Category = new Category();
-// array de tipo Category donde voy a guardar los datos de la BD con el método getCategories()
-categories: Category[] = []
+  product: Product = new Product();
+  category: Category = new Category();
+  // array de tipo Category donde voy a guardar los datos de la BD con el método getCategories()
+  categories: Category[] = [];
 
+  constructor(
+    private router: Router,
+    private productsService: ProductsService,
+    private categoriesService: CategoriesService
+  ) {}
 
-  constructor(private router: Router, private productsService: ProductsService, private categoriesService: CategoriesService) {  }
-
-  ngOnInit(): void{
-    this.categoriesService.getCategories().subscribe(data =>{
-    this.categories = data;
-    })
+  ngOnInit(): void {
+    this.categoriesService.getCategories().subscribe((data) => {
+      this.categories = data;
+    });
   }
 
-  verifyStock(){
-    if (this.product.stock > 0){
+  verifyStock() {
+    if (this.product.stock > 0) {
       this.product.active = true;
     } else {
       this.product.active = false;
     }
   }
 
-  verifyDate(){
-    if(this.product.date_added == null){
-      this.product.date_added = new Date()
+  verifyDate() {
+    if (this.product.date_added == null) {
+      this.product.date_added = new Date();
     }
   }
 
-  async newProduct(){
+  async newProduct() {
     this.verifyStock();
-    this.verifyDate()
-      const product={
-      name: this.product.name, 
+    this.verifyDate();
+    const product = {
+      name: this.product.name,
       stock: this.product.stock,
       price: this.product.price,
       active: this.product.active,
-      date_added : this.product.date_added,
-      fk_category: this.category
-    }
-    if(product.name === undefined || product.stock === undefined || product.price=== undefined || product.fk_category.id === undefined){
-      alert('Todos los campos son obligarios excepto la fecha')
+      date_added: this.product.date_added,
+      fk_category: this.category,
+    };
+    if (
+      this.form.controls["name"].hasError("required") ||
+      this.form.controls["stock"].hasError("required") ||
+      this.form.controls["price"].hasError("required") ||
+      this.form.controls["fk_category"].hasError("required")
+    ) {
+      alert(
+        "Revisa el formulario para asegurarte de que todos los campos son válidos."
+      );
     } else {
       this.productsService.newProduct(product);
       await this.navigateToHome();
     }
-}
+  }
 
-cancelInsert(){
-  this.navigateToHome();
-}
+  cancelInsert() {
+    this.navigateToHome();
+  }
 
-navigateToHome(){
-  this.router.navigate(['/products'])
-}
+  navigateToHome() {
+    this.router.navigate(["/products"]);
+  }
 }
